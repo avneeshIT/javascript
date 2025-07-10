0916d5a81f660722f900d814aa38c71eb7f43c4d
Avneesh Gupta <123972686+avneeshIT@users.noreply.github.com>
2025-07-10 11:02:24 +0000

    variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,24 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+// declare krke chod do to use undefine maante hai
+
+// accountId = 2 // not allowed
+// without var likhe ek variable create kr skte hai
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
