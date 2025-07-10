0a3f51ce2651ac07908487e4e102d72cc1c6062e
Avneesh Gupta <123972686+avneeshIT@users.noreply.github.com>
2025-07-10 14:01:09 +0000

    data

---
+++ 01_basic/datatypes_summary.js
@@ -0,0 +1,42 @@
+// datako kistrah store krte hai and kis trah access krte hai uske basis per data ka categorization kiya gya hai
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+
+// non primtive the return type object hi aata hai
+// array=>object
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+// agr hum function ka type of kre to function hi aaega but vo object function hota hai
+
+console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
